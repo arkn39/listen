148385f6a20d4887b36c55a9fb18e352176d15b5
arkn39 <85568193+arkn39@users.noreply.github.com>
2022-12-13 19:19:53 +0330

    aa

--- js/jq.js
+++ js/jq.js
@@ -42,15 +42,7 @@
         }
     }
     
-    $(function() {
-        $(".navigation-tab-item").click(function() {
-          $(".navigation-tab-item").removeClass("active");
-          $(this).addClass("active");
-          $(".navigation-tab-overlay").css({
-            left: $(this).prevAll().length * 130 + "px"
-          });
-        });
-      });
+ 
 
 
 
